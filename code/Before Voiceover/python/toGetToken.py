b6a748e65123a7a3b328ad88c3cac97d6ccc52eb
tcareless <tyler444c@gmail.com>
2023-09-25 21:17:01 -0400

    Updated toGetToken

--- code/Before Voiceover/python/toGetToken.py
+++ code/Before Voiceover/python/toGetToken.py
@@ -1,47 +1,20 @@
-import os.path
-from google.auth.transport.requests import Request
+import os
 from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
-# If modifying these SCOPES, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
-    creds = None
-
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    token_path = '/home/tyler/Desktop/youtubeProject/code/Before Voiceover/python/token.json'
-
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # If modifying these SCOPES, delete the file token.json.
+    SCOPES = ['https://www.googleapis.com/auth/drive']
     
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '/home/tyler/Desktop/youtubeProject/code/Before Voiceover/python/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+    token_path = '/home/tyler/Desktop/youtubeProject/code/Before Voiceover/python/token.json'
+    credentials_path = '/home/tyler/Desktop/youtubeProject/code/Before Voiceover/python/credentials.json'
 
-        # Save the credentials for the next run
-        with open(token_path, 'w') as token:
-            token.write(creds.to_json())
+    flow = InstalledAppFlow.from_client_secrets_file(
+        credentials_path, SCOPES)
+    creds = flow.run_local_server(port=0)
 
-    # Call the Drive v3 API
-    service = build('drive', 'v3', credentials=creds)
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+    # Save the credentials for the next run
+    with open(token_path, 'w') as token:
+        token.write(creds.to_json())
 
 if __name__ == '__main__':
     main()
